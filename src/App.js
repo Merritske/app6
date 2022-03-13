db985a3b0a4c3cefb4077a335359d94510c0bbc0
An <93704845+Merritske@users.noreply.github.com>
2022-03-13 18:48:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Nieuwe repository maken in Github + in GIT:
+//$ mkdir app6
+
+//$ cd app6
+
+//$ git init
+//Initialized empty Git repository in C:/Users/Gebruiker/app6/.git/
+
+// ~/app6 (master)
+//$ git branch -M main
+
+//~/app6 (main)
+//$ git commit -m "setup"
+//On branch main
+
+//Initial commit
+
+//nothing to commit (create/copy files and use "git add" to track)
+
+//$ git remote add origin https://github.com/Merritske/app6.git
+
+//$ code .
+//nieuwe repository opent in VSCode
+
+
+
+
 function App() {
   return (
     <div className="App">
